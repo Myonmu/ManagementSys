74b99a377d96f28bb8e2f7a13f699b0194335566
Myonmu <62897460+Myonmu@users.noreply.github.com>
2020-05-11 20:53:31 +0200

    mail sender added

---
+++ src/Other/Mail.java
@@ -0,0 +1,62 @@
+package Other;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.swing.JOptionPane;
+
+import java.util.*;
+import javax.activation.*;
+/**
+ * Mail sender
+ * However, the mail sent by the program is often considered spam.
+ * !!!Do not use the mail address provided for spamming.!!!
+ * @author Hippocrate
+ *
+ */
+public class Mail {
+    private final String user="gestion.absence.miskatonic";
+    private final String password="QuickSand3014";
+    private static String host = "smtp.gmail.com";
+    private Properties properties;
+    private Session session;
+    public Mail() {
+    	
+    }
+    /**
+     * Sends a warning mail to student
+     * @param to
+     */
+    public void sendAbsWarning(String to) {
+    	properties=new Properties();
+    	properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        session=Session.getInstance(properties,new javax.mail.Authenticator() {
+            	protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password);
+            }
+        });
+    	try {
+    		MimeMessage message=new MimeMessage(session);
+			message.setFrom(new InternetAddress(user));
+			message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
+			message.setSubject("[ATTENTION] Votre heures d'absence");
+			message.setText("Bonjour, \n "
+					+ "Vous avez depasse la limite de nombre d'heures d'absence.\n"
+					+ "Penalites vous seront attribues.\n\n"
+					+ "Systeme de gestion d'absence");
+			Transport.send(message);
+			JOptionPane.showMessageDialog(null, "Mail(s) envoye(s).");
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
+    public static void main(String[] args) {
+
+    }
+}
